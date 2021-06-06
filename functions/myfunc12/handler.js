6df70fee49fdf1be7c5f70c7499b686905036157
Desno365 <desno365@gmail.com>
2021-06-06 11:34:38 +0200

    Initial commit

---
+++ functions/myfunc12/handler.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = async (event, context) => {
+  const result = {
+    'body': JSON.stringify(event.body),
+    'content-type': event.headers["content-type"],
+    'v': process.version
+  }
+
+  return context
+    .status(200)
+    .succeed(result)
+}
