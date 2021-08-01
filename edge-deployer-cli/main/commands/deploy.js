09ff7aaecf736b1dfbbee6d39f8a8bb554fddad6
Desno365 <desno365@gmail.com>
2021-08-01 18:00:04 +0200

    Add infrastructure and location_id in the environment variables

--- edge-deployer-cli/main/commands/deploy.js
+++ edge-deployer-cli/main/commands/deploy.js
@@ -8,8 +8,8 @@ const infrastructureParser = require('./../utils/infrastructureParser');
 module.exports = function deploy(functionName, infrastructure, {inEvery, inAreas, exceptIn, yaml}) {
     try {
         // Read json file.
-        const data = fs.readFileSync(infrastructure, 'utf8');
-        const infrastructureJson = JSON.parse(data);
+        const infrastructureString = fs.readFileSync(infrastructure, 'utf8');
+        const infrastructureJson = JSON.parse(infrastructureString);
 
         // Check correctness of infrastructure file.
         console.log(chalk.white.bold("🔄 Checking if infrastructure is correct."));
@@ -37,8 +37,8 @@ module.exports = function deploy(functionName, infrastructure, {inEvery, inAreas
 
         // Deploy to all locations.
         for(const location of listOfLocations) {
-            const envVariablesString = "--env=REDIS_HOST=" + location.redis_host + " --env=REDIS_PORT=" + location.redis_port + " --env=REDIS_PASSWORD=" + location.redis_password;
-            console.log(chalk.white.bold("📶 Deploying on " + location.openfaas_gateway + "."));
+            const envVariablesString = "--env=LOCATION_ID=" + location.location_id + " --env=EDGE_INFRASTRUCTURE='" + infrastructureString + "' --env=REDIS_HOST=" + location.redis_host + " --env=REDIS_PORT=" + location.redis_port + " --env=REDIS_PASSWORD=" + location.redis_password;
+            console.log(chalk.white.bold("📶 Deploying on location: \"" + location.location_id + "\", gateway: \"" + location.openfaas_gateway + "\"."));
             shell.exec("echo " + location.openfaas_password + " | faas-cli login --username admin --password-stdin --gateway " + location.openfaas_gateway);
             shell.exec("faas-cli deploy --filter " + functionName + " --yaml " + yaml + " --gateway " + location.openfaas_gateway + " " + envVariablesString);
         }
