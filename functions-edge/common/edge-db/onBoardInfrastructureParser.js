b9052ca76099291604b9505958e3e6ab70039d16
Desno365 <desno365@gmail.com>
2021-09-06 17:05:20 +0200

    Change onBoardInfrastructureParser to be inline with changes in the API

--- functions-edge/common/edge-db/onBoardInfrastructureParser.js
+++ functions-edge/common/edge-db/onBoardInfrastructureParser.js
@@ -1,35 +1,34 @@
 'use strict';
 
 /**
- * Returns the location id of the referring area relative to the location represented by ownLocationId.
+ * Returns the location object of the referring area relative to the location represented by ownLocationId.
  * Assumes that ownLocationId is always present in infrastructureJson.
  * @param infrastructureJson the JSON of the infrastructure.
  * @param ownLocationId the location id of the location that is requesting the referring area.
  * @param referringAreaTypeLevel the level of the referring area type.
  * @returns {*|{}} the location id of the referring area.
  */
-exports.getLocationIdOfReferringAreaInInfrastructure = function(infrastructureJson, ownLocationId, referringAreaTypeLevel) {
+exports.getLocationObjectOfReferringAreaInInfrastructure = function(infrastructureJson, ownLocationId, referringAreaTypeLevel) {
     const areaTypesIdentifiers = infrastructureJson.areaTypesIdentifiers;
     const hierarchy = infrastructureJson.hierarchy;
-    return getLocationIdOfReferringAreaInHierarchy(hierarchy, areaTypesIdentifiers, ownLocationId, referringAreaTypeLevel);
-}
-
-exports.getLocationObject = function(infrastructureJson, locationIdToBeChecked) {
-    const areaTypesIdentifiers = infrastructureJson.areaTypesIdentifiers;
-    const hierarchy = infrastructureJson.hierarchy;
-    return getLocationObjectInHierarchy(hierarchy, areaTypesIdentifiers, locationIdToBeChecked);
+    const listOfParents = getListOfParentsForLocationIdInHierarchy(hierarchy, areaTypesIdentifiers, ownLocationId);
+    console.log("The list of parents for " + ownLocationId + " is: " + listOfParents + ".");
+    const referringAreaId = listOfParents[referringAreaTypeLevel];
+    return getLocationObjectInHierarchy(hierarchy, areaTypesIdentifiers, referringAreaId);
 }
 
 
 // ############################################################################
-//region Location id of the referring area methods.
+//region Location object of the referring area methods.
 // ############################################################################
 
-function getLocationIdOfReferringAreaInHierarchy(hierarchy, areaTypesIdentifiers, ownLocationId, referringAreaTypeLevel) {
-    return getLocationIdOfReferringAreaInAreasContainer(hierarchy, areaTypesIdentifiers, 0, ownLocationId, referringAreaTypeLevel);
+function getListOfParentsForLocationIdInHierarchy(hierarchy, areaTypesIdentifiers, ownLocationId) {
+    const invertedList = getListOfParentsForLocationIdInAreasContainer(hierarchy, areaTypesIdentifiers, 0, ownLocationId);
+    invertedList.reverse();
+    return invertedList;
 }
 
-function getLocationIdOfReferringAreaInAreasContainer(areasContainer, areaTypesIdentifiers, level, ownLocationId, referringAreaTypeLevel) {
+function getListOfParentsForLocationIdInAreasContainer(areasContainer, areaTypesIdentifiers, level, ownLocationId) {
     // Check if it is an areas container or a locations container (last level).
     if(areaTypesIdentifiers.length > level) {
         // It's an areas container.
@@ -37,14 +36,16 @@ function getLocationIdOfReferringAreaInAreasContainer(areasContainer, areaTypesI
             if(areaName === "main-location") {
                 continue;
             }
-            const resultOfLowerLevel = getLocationIdOfReferringAreaInAreasContainer(areasContainer[areaName], areaTypesIdentifiers, level + 1, ownLocationId, referringAreaTypeLevel);
-            console.log("Area " + areaName + " has been searched if it contains the own location id " + ownLocationId + ", the result is: " + resultOfLowerLevel + ".");
-            if(resultOfLowerLevel !== null) {
-                if(referringAreaTypeLevel === level - 1) {
-                    const mainLocationData = areasContainer["main-location"];
-                    console.log("Found the correct location id of the referring area type: " + mainLocationData + ".");
-                    return mainLocationData;
-                } else {
+            if(areaName === ownLocationId) {
+                console.log("Own location found in areas container (id: " + areaName + ", level: " + level + ").");
+                const listOfParents = [];
+                listOfParents.push(areaName);
+                return listOfParents;
+            } else {
+                const resultOfLowerLevel = getListOfParentsForLocationIdInAreasContainer(areasContainer[areaName], areaTypesIdentifiers, level + 1, ownLocationId);
+                console.log("Area " + areaName + " has been searched if it contains the own location id " + ownLocationId + ", the result is: " + resultOfLowerLevel + ".");
+                if(resultOfLowerLevel !== null) {
+                    resultOfLowerLevel.push(areaName);
                     return resultOfLowerLevel;
                 }
             }
@@ -52,36 +53,27 @@ function getLocationIdOfReferringAreaInAreasContainer(areasContainer, areaTypesI
         return null;
     } else {
         // It's actually a locations container (each field in areasContainer is a location object).
-        return checkIfOwnLocationIsInLocationsContainer(areasContainer, level, ownLocationId);
+        return getListOfParentsForLocationIdInLocationsContainer(areasContainer, level, ownLocationId);
     }
 }
 
-function checkIfOwnLocationIsInLocationsContainer(locationsContainer, level, ownLocationId) {
+function getListOfParentsForLocationIdInLocationsContainer(locationsContainer, level, ownLocationId) {
     for(const locationName in locationsContainer) {
-        if(locationName === "main-location") {
-            continue;
-        }
         if(locationName === ownLocationId) {
-            console.log("Own location found (id: " + locationName + ", level: " + level + ").");
-            const mainLocationData = locationsContainer["main-location"];
-            console.log("Since the own location has been found in the locations container we are returning the main-location of it that is " + mainLocationData + " (may be overwritten later).");
-            return mainLocationData;
+            console.log("Own location found in locations container (id: " + locationName + ", level: " + level + ").");
+            const listOfParents = [];
+            listOfParents.push(locationName);
+            return listOfParents;
         }
     }
     return null;
 }
-//endregion
 
-
-// ############################################################################
-//region Get location object methods.
-// ############################################################################
-
-function getLocationObjectInHierarchy(hierarchy, areaTypesIdentifiers, locationIdToBeChecked) {
-    return getLocationObjectInAreasContainer(hierarchy, areaTypesIdentifiers, 0, locationIdToBeChecked);
+function getLocationObjectInHierarchy(hierarchy, areaTypesIdentifiers, locationId) {
+    return getLocationObjectInAreasContainer(hierarchy, areaTypesIdentifiers, 0, locationId);
 }
 
-function getLocationObjectInAreasContainer(areasContainer, areaTypesIdentifiers, level, locationIdToBeChecked) {
+function getLocationObjectInAreasContainer(areasContainer, areaTypesIdentifiers, level, locationId) {
     // Check if it is an areas container or a locations container (last level).
     if(areaTypesIdentifiers.length > level) {
         // It's an areas container.
@@ -89,29 +81,32 @@ function getLocationObjectInAreasContainer(areasContainer, areaTypesIdentifiers,
             if(areaName === "main-location") {
                 continue;
             }
-            const resultOfLowerLevel = getLocationObjectInAreasContainer(areasContainer[areaName], areaTypesIdentifiers, level + 1, locationIdToBeChecked);
-            console.log("Area " + areaName + " has been searched if it contains the location " + locationIdToBeChecked + ", result: " + (resultOfLowerLevel !== null) + ".");
-            if(resultOfLowerLevel !== null)
-                return resultOfLowerLevel;
+            if(areaName === locationId) {
+                console.log("Location found in areas container (id: " + areaName + ", level: " + level + ").");
+                const locationObject = areasContainer[areaName]["main-location"];
+                locationObject.location_id = locationId;
+                return locationObject;
+            } else {
+                const resultOfLowerLevel = getLocationObjectInAreasContainer(areasContainer[areaName], areaTypesIdentifiers, level + 1, locationId);
+                console.log("Area " + areaName + " has been searched if it contains the location id " + locationId + ", the result is: " + resultOfLowerLevel + ".");
+                if(resultOfLowerLevel !== null) {
+                    return resultOfLowerLevel;
+                }
+            }
         }
         return null;
     } else {
         // It's actually a locations container (each field in areasContainer is a location object).
-        const resultOfLocationsLevel = getLocationObjectInLocationsContainer(areasContainer, level, locationIdToBeChecked);
-        console.log("A locations container has been searched if it contains the location " + locationIdToBeChecked + ", result: " + (resultOfLocationsLevel !== null)+ ".");
-        return resultOfLocationsLevel;
+        return getLocationObjectInLocationsContainer(areasContainer, level, locationId);
     }
 }
 
-function getLocationObjectInLocationsContainer(locationsContainer, level, locationIdToBeChecked) {
+function getLocationObjectInLocationsContainer(locationsContainer, level, locationId) {
     for(const locationName in locationsContainer) {
-        if(locationName === "main-location") {
-            continue;
-        }
-        if(locationName === locationIdToBeChecked) {
+        if(locationName === locationId) {
+            console.log("Location found in locations container (id: " + locationName + ", level: " + level + ").");
             const locationObject = locationsContainer[locationName];
-            locationObject.location_id = locationName;
-            console.log("Location " + locationName + " is the location searched and it has a level of " + level + ".");
+            locationObject.location_id = locationId;
             return locationObject;
         }
     }
